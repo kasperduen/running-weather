7afbe73b4bf37ff3ece322dae380f4431fed2852
Kasper Due <kasp9023@hotmail.com>
2021-02-06 19:41:00 +0100

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./styles/App.css";
+import "./styles/weather-icons.min.css";
+import "./styles/weather-icons-wind.min.css";
+import axios from "axios";
+
+const apiKey = "35ea9aa24779e05fcd12b7300dd7e6e3";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // 35ea9aa24779e05fcd12b7300dd7e6e3
+    const [position, setPosition] = useState({ lat: null, long: null });
+
+    useEffect(() => {
+        getWeather();
+    }, [position]);
+
+    useEffect(() => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        }
+    }, []);
+
+    const getWeather = async () => {
+        if (!position) {
+            return;
+        }
+        const weather = await axios.get(
+            `https://api.openweathermap.org/data/2.5/onecall?lat=${position?.lat}&lon=${position?.long}&exclude=minutely,alerts&appid=${apiKey}&units=metric`
+        );
+        console.log({ weather });
+    };
+
+    function showPosition(position: any) {
+        const { latitude, longitude } = position.coords;
+        setPosition({ lat: latitude, long: longitude });
+    }
+
+    return (
+        <div className="App">
+            <div className="weather-card">
+                <h1>Running Weather</h1>
+                <div className="weather-summary">
+                    <h3></h3>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
